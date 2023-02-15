ba4af83f9b7f298c63e7772badfc13cc0c5cbc40
Ignacio Bruno <ignaciobruno90@gmail.com>
2023-02-15 19:19:51 -0300

    Trabajo de calculadora de cargos del Club Atlético San Lorenzo de Almagro. Segunda Pre Entrega Coderhouse

--- main.js
+++ main.js
@@ -149,19 +149,17 @@ console.log("Se contabilizaron un total de " + votosTotales + " votos totales.")
 
 //Busca cual es la lista que sacó mayores votos.
 let votosMayor = Math.max(lista1.votos, lista2.votos, lista3.votos, lista4.votos, lista5.votos);
-console.log(votosMayor);
 
 //Localiza la lista ganadora dentro del arrayListas, y la almacena en la variable listaGanadora.
-let listaGanadora = arrayListas.filter (function (arrayListas){
+let listaGanadora = arrayListas.find (function (arrayListas){
     return arrayListas.votos == votosMayor;
 });
 
-console.log(listaGanadora);
+console.log(listaGanadora.candidato + " de la agrupación " + listaGanadora.nombre + " ganó las elecciones con " + listaGanadora.votos + " votos.");
 
 //Se calcula el porcentaje que obtiene la mayoría.
 
 let porcentajeMayoria = parseFloat((votosMayor*100)/votosTotales).toFixed(3);
-console.log("El porcentaje de la mayoría es: " + porcentajeMayoria);
 
 //Se calcula el porcentaje que obtiene la minoría.
 
@@ -169,10 +167,22 @@ let votosMinoria = (votosTotales-votosMayor)-votosBlancos;
 console.log("Los votos totales de la minoría son: " + votosMinoria);
 
 let porcentajeMinoria = parseFloat((votosMinoria*100)/votosTotales).toFixed(3);
-console.log("El porcentaje de la minoría es: " + porcentajeMinoria);
 
 console.log("La mayoría obtuvo un " + porcentajeMayoria + " porciento.");
 console.log("La minoría obtuvo un " + porcentajeMinoria + " porciento.");
+
+//Se calculan los porcentajes para cada una de las listas.
+let porcentajeLista1 = parseFloat((lista1.votos*100)/votosTotales).toFixed(3);
+let porcentajeLista2 = parseFloat((lista2.votos*100)/votosTotales).toFixed(3);
+let porcentajeLista3 = parseFloat((lista3.votos*100)/votosTotales).toFixed(3);
+let porcentajeLista4 = parseFloat((lista4.votos*100)/votosTotales).toFixed(3);
+let porcentajeLista5 = parseFloat((lista5.votos*100)/votosTotales).toFixed(3);
+
+console.log(lista1.candidato + " (" + lista1.nombre + ") obtuvo el " + porcentajeLista1 + "% de los votos.");
+console.log(lista2.candidato + " (" + lista2.nombre + ") obtuvo el " + porcentajeLista2 + "% de los votos.");
+console.log(lista3.candidato + " (" + lista3.nombre + ") obtuvo el " + porcentajeLista3 + "% de los votos.");
+console.log(lista4.candidato + " (" + lista4.nombre + ") obtuvo el " + porcentajeLista4 + "% de los votos.");
+console.log(lista5.candidato + " (" + lista5.nombre + ") obtuvo el " + porcentajeLista5 + "% de los votos.");
 
 //Se calculan los cargos en Asamblea de Representantes y Comisión Directiva que ocupa la mayoría.
 
@@ -192,6 +202,8 @@ if (miembrosMayoriaAR > 75){
     miembrosMayoriaAR = 90-15
 }
 
+
+
 console.log("La mayoría obtuvo " + miembrosMayoriaAR + " asambleístas.");
 console.log("La mayoría obtuvo " + miembrosMayoriaCD + " cargos en Comisión Directiva.");
 
@@ -204,3 +216,120 @@ let miembrosMinoriaCD = parseFloat(20 - miembrosMayoriaCD).toFixed(3);
 
 console.log("La minoría obtiene " + miembrosMinoriaAR + " asambleístas.");
 console.log("La minoría obtiene " + miembrosMinoriaCD + " cargos en Comisión Directiva");
+
+// //Segundo: se define el cociente para la distribución de los cargos entre la minoría.
+let cocienteMinoriaAR
+let cocienteMinoriaCD
+
+if (porcentajeMinoria > 50) {
+    cocienteMinoriaAR = votosMinoria/miembrosMinoriaAR;
+    cocienteMinoriaCD = votosMinoria/miembrosMinoriaCD;
+} else {
+    cocienteMinoriaAR = (votosMayor+votosMinoria)/miembrosMinoriaAR;
+    cocienteMinoriaCD = (votosMayor+votosMinoria)/miembrosMinoriaCD;
+}
+
+console.log("El cociente para calcular la distribución de cargos de la minoria para la Asamblea es: " + cocienteMinoriaAR);
+
+console.log("El cociente para calcular la distribución de cargos de la minoría para la Comisión Directiva es: " + cocienteMinoriaCD);
+
+// //Tercero: Se calculan los cargos para cada agrupación minoritaria a partir de la relación entre votos obtenidos y el cociente.
+
+// //ASAMBLEA DE REPRESENTANTES
+
+//Se ordena el array en base a la cantidad de votos obtenidos por las listas.
+arrayListas.sort((x, y) => y.votos - x.votos);
+console.log(arrayListas);
+
+//Se hace un switch para seleccionar cuantas listas se le calcula los asambleístas
+
+let miembrosLista2AR
+let miembrosLista3AR
+let miembrosLista4AR
+let miembrosLista5AR
+
+switch(listas){
+    case 2:
+        miembrosLista2AR = parseFloat(arrayListas[1].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2AR + " asambleístas.");
+    break;
+
+    case 3:
+        miembrosLista2AR = parseFloat(arrayListas[1].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2AR + " asambleístas.");
+
+        miembrosLista3AR = parseFloat(arrayListas[2].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[2].nombre + " obtiene " + miembrosLista3AR + " asambleístas.");
+    break;
+
+    case 4:
+        miembrosLista2AR = parseFloat(arrayListas[1].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2AR + " asambleístas.");
+
+        miembrosLista3AR = parseFloat(arrayListas[2].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[2].nombre + " obtiene " + miembrosLista3AR + " asambleístas.");
+
+        miembrosLista4AR = parseFloat(arrayListas[3].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[3].nombre + " obtiene " + miembrosLista4AR + " asambleístas.");
+    break;
+
+    case 5:
+        miembrosLista2AR = parseFloat(arrayListas[1].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2AR + " asambleístas.");
+
+        miembrosLista3AR = parseFloat(arrayListas[2].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[2].nombre + " obtiene " + miembrosLista3AR + " asambleístas.");
+
+        miembrosLista4AR = parseFloat(arrayListas[3].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[3].nombre + " obtiene " + miembrosLista4AR + " asambleístas.");
+
+        miembrosLista5AR = parseFloat(arrayListas[4].votos/cocienteMinoriaAR).toFixed(3);
+        console.log(arrayListas[4].nombre + " obtiene " + miembrosLista5AR + " asambleístas.");
+    break;
+}
+
+// //COMISIÓN DIRECTIVA
+let miembrosLista2CD
+let miembrosLista3CD
+let miembrosLista4CD
+let miembrosLista5CD
+
+switch(listas){
+    case 2:
+        miembrosLista2CD = parseFloat(arrayListas[1].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+    break;
+
+    case 3:
+        miembrosLista2CD = parseFloat(arrayListas[1].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+
+        miembrosLista3CD = parseFloat(arrayListas[2].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[2].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+    break;
+
+    case 4:
+        miembrosLista2CD = parseFloat(arrayListas[1].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+
+        miembrosLista3CD = parseFloat(arrayListas[2].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[2].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+
+        miembrosLista4CD = parseFloat(arrayListas[3].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[3].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+    break;
+
+    case 5:
+        miembrosLista2CD = parseFloat(arrayListas[1].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[1].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+
+        miembrosLista3CD = parseFloat(arrayListas[2].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[2].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+
+        miembrosLista4CD = parseFloat(arrayListas[3].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[3].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+
+        miembrosLista5CD = parseFloat(arrayListas[4].votos/cocienteMinoriaCD).toFixed(3);
+        console.log(arrayListas[4].nombre + " obtiene " + miembrosLista2CD + " miembros en Comisión Directiva.");
+    break;
+}
